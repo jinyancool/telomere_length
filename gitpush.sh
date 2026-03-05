0b2d2243b9e7c65dd7dfb109825de391987b4e39
Jinyan <jinyancool@163.com>
2026-03-05 15:33:15 +0800

    telomere length study

--- gitpush.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-COMMIT_MSG=${1-"telomere length study"}
-BRANCH=${2-"main"}
-# Usage: ./gitpush.sh "Your commit message" "branch-name"
-git add .
-git commit -m "$COMMIT_MSG"
-git push origin $BRANCH
